c7f56d48e1dcc7f9cca236652724229cc63878d0
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-04-05 11:23:14 -0300

    modificated name and authorized cors

---
+++ src/main/java/com/bookstore/ai/controllers/ChatGptApi.java
@@ -0,0 +1,23 @@
+package com.bookstore.ai.controllers;
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/bookstore")
+public class ChatGptApi {
+    @Autowired
+    private OpenAiChatClient chatClient;
+
+    @PostMapping("/ask")
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
+    public ChatResponse askQuestion(@RequestBody String question){
+        return chatClient.call(new Prompt(question));
+    }
+
+}
